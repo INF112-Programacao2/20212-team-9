b21f7c0e279d24e92011990cd6728b3e7b30441c
jefte gomes <98670424+kalupx@users.noreply.github.com>
2022-03-15 16:05:40 -0300

    Add files via upload
    main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+
+using namespace std;
+
+const int LARGURA_TELA = 640;
+const int ALTURA_TELA = 480;
+
+int main(){
+
+
+    ALLEGRO_DISPLAY *janela = NULL;
+    ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+    ALLEGRO_BITMAP *botao_sair = NULL, *area_central = 0;
+    
+    int sair = 0;
+    cout << "Hello World" << std::endl;
+    
+    return 0;
+}
