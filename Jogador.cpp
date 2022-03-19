5c7a95da16282a398633ab36d8c72a5b24b798da
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2022-03-19 00:35:09 -0300

    Modificações na classe jogador

--- Jogador.cpp
+++ Jogador.cpp
@@ -1,9 +1,17 @@
 #include "Jogador.h"
 
+int Jogador::get_vida_do_castelo() {
+    return _vida_do_castelo;
+}
+
+void Jogador::set_vida_do_castelo(int v) {
+    _vida_do_castelo += v;
+}
+
 int Jogador::get_dinheiro() {
     return _dinheiro;
 }
 
 void Jogador::set_dinheiro(int n) {
-    _dinheiro = n;
+    _dinheiro += n;
 }
--- Jogador.h
+++ Jogador.h
@@ -3,11 +3,14 @@
 
 class Jogador {
 private:
-    int _dinheiro;
+    int _vida_do_castelo = 100;
+    int _dinheiro = 100;
 
 public:
+    int get_vida_do_castelo();
+    void set_vida_do_castelo(int v);
     int get_dinheiro();
-    void set_dinheiro(int n);
+    void set_dinheiro(int d);
 };
 
 #endif
