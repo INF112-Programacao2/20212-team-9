aa35bac1c561f8d858d3deb2ff668960d8123432
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2022-03-19 00:10:30 -0300

    Criação da classe Jogador (.h e .cpp)

---
+++ Jogador.cpp
@@ -0,0 +1,9 @@
+#include "Jogador.h"
+
+int Jogador::get_dinheiro() {
+    return _dinheiro;
+}
+
+void Jogador::set_dinheiro(int n) {
+    _dinheiro = n;
+}
---
+++ Jogador.h
@@ -0,0 +1,13 @@
+#ifndef JOGADOR
+#define JOGADOR
+
+class Jogador {
+private:
+    int _dinheiro;
+
+public:
+    int get_dinheiro();
+    void set_dinheiro(int n);
+};
+
+#endif
